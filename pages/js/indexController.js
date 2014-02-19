332088f937e56b4df5dfa023bb7d7508163250ca
soidid <soidid@gmail.com>
2014-02-19 12:34:35 +0800

    add explanation in index. dynamically adjust height.

--- pages/js/indexController.js
+++ pages/js/indexController.js
@@ -6,15 +6,18 @@ askControllers.controller('indexCtrl', ['$scope', 'candidateService', function($
 
  
   $scope.candidates = candidateService;
+
+  
   $scope.tabChoose = function(chosed_tab){
   	
   	$(".tab").hide();
   	$(".index_tab").removeClass("index_tab_active");
   	$("#"+chosed_tab).show();
   	$("#"+chosed_tab+"_tab").addClass("index_tab_active");
+    
+    //dynamically adjust height
+    var height = $("#"+chosed_tab).height()+150;
+    $("#why_and_how_segment").css("height",height+"px");
   }
  
-
-
-
 }]);
