874588be42aaa14f17aeeef228448cb171be844e
Pomin Wu <pomin5@gmail.com>
2014-02-19 14:56:08 +0800

    re-indent

--- pages/js/questionDetailController.js
+++ pages/js/questionDetailController.js
@@ -1,6 +1,4 @@
-askControllers.controller('questionDetailCtrl', ['$scope', 'candidateService', function($scope, candidateService){
-
-
+askControllers.controller('questionDetailCtrl', ['$scope', '$routeParams', 'candidateService', 'questionService', function($scope, $routeParams, candidateService, questionService){
 
   semanticMenuReady();
   semanticAccordingReady();
@@ -9,27 +7,21 @@ askControllers.controller('questionDetailCtrl', ['$scope', 'candidateService', f
   $scope.data = global.questionData[0];
 
   $scope.goToTop = function(){
-
-     var body = $("html, body");
-     body.animate({scrollTop:0}, '500', 'swing');
+    var body = $("html, body");
+    body.animate({scrollTop:0}, '500', 'swing');
   };
-  
 
   // for sign question area keep floating
   var nav = $('#sign-question');
-
-    $(window).scroll(function () {
-      
-        if ($(this).scrollTop() > 230) {
-            nav.addClass("f-nav");
-            nav.removeClass("f-fix");
-
-        } else {
-            nav.removeClass("f-nav");
-            nav.addClass("f-fix");
-
-        }
-    });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 230) {
+      nav.addClass("f-nav");
+      nav.removeClass("f-fix");
+    } else {
+      nav.removeClass("f-nav");
+      nav.addClass("f-fix");
+    }
+  });
  
 }]);
 
